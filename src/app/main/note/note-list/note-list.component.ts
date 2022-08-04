42b1823c81616c1ea8d3d6f5e94d63227d361a19
Thanh Nguy <vanthanhnguy2907@gmail.com>
2022-08-04 21:35:05 +0700

    create note component

--- src/app/main/note/note-list/note-list.component.ts
+++ src/app/main/note/note-list/note-list.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-note-list',
   templateUrl: './note-list.component.html',
-  styleUrls: ['./note-list.component.scss']
+  styleUrls: ['./note-list.component.scss'],
 })
 export class NoteListComponent implements OnInit {
+  noteTitle: string = 'Hehehe';
+  noteContent: string = 'Lalala';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/main/note/note.module.ts
+++ src/app/main/note/note.module.ts
@@ -5,10 +5,17 @@ import { NoteListComponent } from './note-list/note-list.component';
 import { NoteItemComponent } from './note-item/note-item.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [NoteListComponent, NoteItemComponent],
-  imports: [BrowserModule, RouterModule, MatCardModule, MatButtonModule],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+  ],
   exports: [NoteListComponent, NoteItemComponent],
   providers: [],
 })
