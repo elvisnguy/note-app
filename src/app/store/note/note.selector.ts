a55b704d4639a93629b13d39122ad23a79361272
Thanh Nguy <vanthanhnguy2907@gmail.com>
2022-08-08 17:18:38 +0700

    add search note by label title

--- src/app/store/note/note.selector.ts
+++ src/app/store/note/note.selector.ts
@@ -1,11 +1,14 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as fromReducer from './note.reducer';
+import { cloneDeep } from 'lodash';
 
 export const selectNotesState =
   createFeatureSelector<fromReducer.NoteState>('notes');
 
 export const selectNotes = createSelector(selectNotesState, (state) => {
-  return state.notes.slice(0, 10);
+  return cloneDeep(state.notes)
+    .sort((a, b) => Number(b.id) - Number(a.id))
+    .slice(0, 10);
 });
 
 export const selectNoteDetailsById = (id: number) =>
